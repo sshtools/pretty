6d4a6f0ac67f8dfe992eee60577bcc10a8c77253
Brett Smith <brett@logonbox.com>
2026-02-23 14:14:54 +0000

    Tests

--- src/test/scripts/iterm2-image.sh
+++ src/test/scripts/iterm2-image.sh
@@ -3,7 +3,7 @@ if [ $# -eq 0 ]; then
   echo "Usage: divider file"
   exit 1
 fi
-printf '\033]1337;File=inline=1;width=50%%;height=10;preserveAspectRatio=0'
+printf '\033]1337;File=inline=1;width=50%%;height=50%%;preserveAspectRatio=1'
 printf ":"
 base64 -w 0 < "$1"
 printf '\a\n'
